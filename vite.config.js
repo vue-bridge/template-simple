1b6ff171e8b0256b467fe6bbd12a7d9b18890391
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-09-22 13:28:40 +0200

    clean up

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+/**
+ * @type {import('vite').UserConfig}
+ */
 export default defineConfig({
   plugins: [vue()],
   resolve: {
