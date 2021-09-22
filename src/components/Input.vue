d1a3f1c81d28765f64d5337bffab83d99ab431ba
Thorsten Luenborg <t.luenborg@googlemail.com>
2021-09-22 10:55:27 +0200

    Batman! This commit has no parents

---
+++ src/components/Input.vue
@@ -0,0 +1,32 @@
+<script>
+import { defineComponent, attrsListenersMixin } from '@vue-bridge/runtime'
+
+export default defineComponent({
+  name: 'Input',
+  mixins: [attrsListenersMixin()],
+  props: {
+    modelValue: {
+      type: String,
+    },
+  },
+  emits: ['update:modelValue'],
+
+  computed: {
+    model: {
+      get() {
+        return this.modelValue
+      },
+      set(v) {
+        this.$emit('update:modelValue', v)
+      },
+    },
+  },
+})
+</script>
+<template>
+  <div>
+    <label for="input">
+      <input type="text" v-model="model" v-bind="$_attrs" />
+    </label>
+  </div>
+</template>
